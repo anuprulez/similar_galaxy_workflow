6c2be2c45fbc818e5fbe4f833b738013a5cf9c66
anuprulez <anup.rulez@gmail.com>
2018-03-14 17:12:49 +0100

    Update the network

--- plot_graphs.py
+++ plot_graphs.py
@@ -16,28 +16,40 @@ def plot_data_distribution( file_path ):
     plt.show()
 
 
-def plot_loss( file_path ):
-    loss_values = list()
-    with open( file_path, 'r' ) as loss_file:
-        loss_values = loss_file.read().split( "\n" )
-    loss_values = [ float( item ) for item in loss_values if item ]   
-    plt.plot( loss_values )
+def plot_loss( file_path_train, file_path_test ):
+    loss_values_train = list()
+    loss_values_test = list()
+    with open( file_path_train, 'r' ) as loss_file_train:
+        loss_values_train = loss_file_train.read().split( "\n" )
+    loss_values_train = [ float( item ) for item in loss_values_train if item ]
+    with open( file_path_test, 'r' ) as loss_file_test:
+        loss_values_test = loss_file_test.read().split( "\n" )
+    loss_values_test = [ float( item ) for item in loss_values_test if item ]   
+    plt.plot( loss_values_train )
+    plt.plot( loss_values_test )
     plt.ylabel( 'Loss' )
     plt.xlabel( 'Epochs' )
     plt.title( 'Loss drop vs epochs' )
+    plt.legend( [ "train", "test" ] )
     plt.grid( True )
     plt.show()
 
 
-def plot_accuracy( file_path ):
-    acc_values = list()
-    with open( file_path, 'r' ) as acc_file:
-        acc_values = acc_file.read().split( "\n" )
-    acc_values = [ float( item ) for item in acc_values if item ]   
-    plt.plot( acc_values )
+def plot_accuracy( file_path_train, file_path_test ):
+    acc_values_train = list()
+    acc_values_test = list()
+    with open( file_path_train, 'r' ) as acc_file_train:
+        acc_values_train = acc_file_train.read().split( "\n" )
+    acc_values_train = [ float( item ) for item in acc_values_train if item ]
+    with open( file_path_test, 'r' ) as acc_file_test:
+        acc_values_test = acc_file_test.read().split( "\n" )
+    acc_values_test = [ float( item ) for item in acc_values_test if item ]   
+    plt.plot( acc_values_train )
+    plt.plot( acc_values_test )
     plt.ylabel( 'Accuracy' )
     plt.xlabel( 'Epochs' )
-    plt.title( 'Accuracy vs epochs' )
+    plt.title( 'Accuracy vs training epochs' )
+    plt.legend( [ "train", "test" ] )
     plt.grid( True )
     plt.show()
 
@@ -55,7 +67,6 @@ def plot_top_prediction( file_path ):
 
 
 #plot_data_distribution( "data/data_distribution.txt" )
-plot_loss( "data/loss_history.txt" )
-plot_accuracy( "data/accuracy_history.txt" )
-plot_accuracy( "data/validation_accuracy.txt" )
-plot_top_prediction( "data/top_pred.txt" )
+plot_loss( "data/loss_history.txt", "data/val_loss_history.txt" )
+plot_accuracy( "data/accuracy_history.txt", "data/val_accuracy_history.txt" )
+#plot_top_prediction( "data/top_pred.txt" )
