Fix issue with masking 0th position

plot_graphs.py
@@ -41,33 +41,19 @@ def plot_labels_distribution( test_path, train_path ):
     train_seq_count.extend( test_seq_count )
     train_labels_count.extend( test_labels_count )
     comp_labels_index = np.arange( len( train_labels_count ) )
-    print len( comp_labels_index )
-    print len( train_seq_count )
-    print len( train_labels_count )
     font = { 'family' : 'sans serif', 'size': 22 }
     plt.rc('font', **font) 
-<<<<<<< HEAD
     plt.bar( np.arange( len( test_seq_count ) ), test_labels_count, facecolor='r', align='center' )
     plt.xlabel( 'Number of samples' )
     plt.ylabel( 'Number of next compatible tools in samples' )
-=======
-    plt.bar( comp_labels_index, test_labels_count, facecolor='r', align='center' )
-    plt.xlabel( 'Number of samples' )
-    plt.ylabel( 'Number of tools in samples' )
->>>>>>> 34b5afa6b5d578ded9c3dc20f8d9046becc71339
     plt.title( 'Distribution of number of next tools in test samples' )
     plt.grid( True )
     plt.show()
 
     '''plt.bar( comp_labels_index, train_labels_count, facecolor='r', align='center' )
     plt.xlabel( 'Number of samples' )
-<<<<<<< HEAD
     plt.ylabel( 'Number of next compatible tools in samples' )
     plt.title( 'Distribution of number of next tools in train samples' )
-=======
-    plt.ylabel( 'Number of labels in samples' )
-    plt.title( 'Distribution of number of next tools in test samples' )
->>>>>>> 34b5afa6b5d578ded9c3dc20f8d9046becc71339
     plt.grid( True )
     plt.show()'''
 
@@ -125,7 +111,6 @@ def plot_top_prediction( abs_file_path ):
     plt.grid( True )
     plt.show()
 
-<<<<<<< HEAD
 def plot_next_tools_precision( file_path ):
     next_tools = list()
     precision = list()
@@ -141,13 +126,22 @@ def plot_next_tools_precision( file_path ):
     plt.title( 'Number of next tool vs precision' )
     plt.grid( True )
     plt.show()
-=======
-#plot_loss( "data/loss_history.txt", "data/val_loss_history.txt" )
-#plot_accuracy( "data/abs_top_pred.txt", "data/test_top_pred.txt" )
-plot_labels_distribution( "data/test_data_labels_dict.txt", "data/train_data_labels_dict.txt" )
->>>>>>> 34b5afa6b5d578ded9c3dc20f8d9046becc71339
 
+def plot_tools_compatible_tools( file_path ):
+    next_tools = list()
+    with open( file_path, 'r' ) as file_next_tools:
+        next_tools_list = json.loads( file_next_tools.read() )
+    for tool in next_tools_list:
+        next_tools.append( len( next_tools_list[ tool ].split( "," ) ) )
+    plt.bar( np.arange( len( next_tools ) ), next_tools )
+    plt.ylabel( 'Number of next compatible tools' )
+    plt.xlabel( 'Tools' )
+    plt.title( 'Distribution of next compatible tools for all the tools' )
+    plt.grid( True )
+    plt.show()
+
+plot_tools_compatible_tools( "data/compatible_tools.json" )
 #plot_loss( "data/loss_history.txt", "data/val_loss_history.txt" )
 #plot_accuracy( "data/abs_top_pred.txt", "data/test_top_pred.txt" )
 #plot_labels_distribution( "data/test_data_labels_dict.txt", "data/train_data_labels_dict.txt" )
-plot_next_tools_precision( "data/test_data_performance_10.csv" )
+#plot_next_tools_precision( "data/test_data_performance_10.csv" )

predict_next_tool.py
@@ -35,28 +35,24 @@ class PredictNextTool:
         self.test_top_compatibility_pred_path = self.current_working_dir + "/data/test_top_compatible_pred.txt"
 
     @classmethod
-    def evaluate_LSTM_network( self ):
+    def evaluate_LSTM_network( self, n_epochs=30, batch_size=40, dropout=0.1, lstm_units=64, embedding_vec_size=200, lr=0.01, reg_coeff=0.01 ):
         """
         Create LSTM network and evaluate performance
         """
         print ( "Dividing data..." )
-        n_epochs = 10
-        batch_size = 40
-        dropout = 0.25
-        lstm_units = 64
         # get training and test data and their labels
         data = prepare_data.PrepareData()
         train_data, train_labels, test_data, test_labels, dictionary, reverse_dictionary, next_compatible_tools = data.get_data_labels_mat()
-        dimensions = len( dictionary )
-        embedding_vec_size = 200
-        optimizer = RMSprop( lr=0.01 )
+        # Increase the dimension by 1 to mask the 0th position
+        dimensions = len( dictionary ) + 1
+        optimizer = RMSprop( lr=lr )
         # define recurrent network
         model = Sequential()
         model.add( Embedding( dimensions, embedding_vec_size, mask_zero=True ) )
         model.add( SpatialDropout1D( dropout ) )
         model.add( LSTM( lstm_units, dropout=dropout, return_sequences=True, recurrent_dropout=dropout, activation='softsign' ) )
         model.add( LSTM( lstm_units, dropout=dropout, return_sequences=False, recurrent_dropout=dropout, activation='softsign' ) )
-        model.add( Dense( dimensions, activation='sigmoid', activity_regularizer=regularizers.l2( 0.01 ) ) )
+        model.add( Dense( dimensions, activation='sigmoid', activity_regularizer=regularizers.l2( reg_coeff ) ) )
         model.compile( loss="binary_crossentropy", optimizer=optimizer )
         # save the network as json
         model_json = model.to_json()
@@ -111,9 +107,9 @@ class PredictCallback( Callback ):
             prediction_pos = np.argsort( prediction, axis=-1 )
             topk_prediction_pos = prediction_pos[ -topk: ]
             # read tool names using reverse dictionary
-            sequence_tool_names = [ reverse_data_dictionary[ int( tool_pos ) + 1 ] for tool_pos in test_sample_tool_pos ]
-            actual_next_tool_names = [ reverse_data_dictionary[ int( tool_pos ) + 1 ] for tool_pos in actual_classes_pos ]
-            top_predicted_next_tool_names = [ reverse_data_dictionary[ int( tool_pos ) + 1 ] for tool_pos in topk_prediction_pos ]
+            sequence_tool_names = [ reverse_data_dictionary[ int( tool_pos ) ] for tool_pos in test_sample_tool_pos ]
+            actual_next_tool_names = [ reverse_data_dictionary[ int( tool_pos ) ] for tool_pos in actual_classes_pos ]
+            top_predicted_next_tool_names = [ reverse_data_dictionary[ int( tool_pos ) ] for tool_pos in topk_prediction_pos ]
             # find false positives
             false_positives = [ tool_name for tool_name in top_predicted_next_tool_names if tool_name not in actual_next_tool_names ]
             absolute_precision = 1 - ( len( false_positives ) / float( len( actual_next_tool_names ) ) )

prepare_data.py
@@ -166,24 +166,23 @@ class PrepareData:
         """
         size_data = len( paths_dictionary )
         data_mat = np.zeros( [ size_data, self.max_tool_sequence_len ] )
-        label_mat = np.zeros( [ size_data, num_classes ] )
+        label_mat = np.zeros( [ size_data, num_classes + 1 ] )
         train_counter = 0
         for train_seq, train_label in list( paths_dictionary.items() ):
             positions = train_seq.split( "," )
             start_pos = self.max_tool_sequence_len - len( positions )
             for id_pos, pos in enumerate( positions ):
-                data_mat[ train_counter ][ start_pos + id_pos ] = int( pos ) - 1
+                data_mat[ train_counter ][ start_pos + id_pos ] = int( pos )
             for label_item in train_label.split( "," ):
-                label_mat[ train_counter ][ int( label_item ) - 1 ] = 1.0
+                label_mat[ train_counter ][ int( label_item ) ] = 1.0
             train_counter += 1
         return data_mat, label_mat
 
     @classmethod
     def assign_filetype_compatibility( self, filetypes_path, dictionary ):
         """
-        Get the tools with compatible file types
+        Get the next tools with compatible file types for each tool
         """
-        max_compatible = 100
         with open( filetypes_path, "r" ) as file_types:
             tools_filetypes = json.loads( file_types.read() )
         tools_compatibility = dict()
@@ -200,7 +199,7 @@ class PrepareData:
                                 if out_tool in dictionary and in_tool in dictionary:
                                     if in_tool not in compatible_next_tools:
                                         compatible_next_tools.append( in_tool )
-            tools_compatibility[ out_tool ] = ",".join( compatible_next_tools[ :max_compatible ] )
+            tools_compatibility[ out_tool ] = ",".join( compatible_next_tools )
         with open( self.compatible_tools_filetypes, "w" ) as compatible_filetypes:
             compatible_filetypes.write( json.dumps( tools_compatibility ) )
         return tools_compatibility
@@ -221,9 +220,8 @@ class PrepareData:
         test_paths = raw_paths[ :int( test_share ) ]
         train_paths = raw_paths[ int( test_share ): ]
         random.shuffle( train_paths )
-        print( "Processing train paths..." )
+        # process training and test paths in different ways
         self.process_train_paths( train_paths, dictionary )
-        print( "Processing test paths..." )
         self.process_test_paths( test_paths, dictionary )
         # create sequences with labels for train and test paths
         train_paths_dict = self.prepare_paths_labels_dictionary( self.train_file, self.train_data_labels_dict, next_compatible_tools, dictionary, reverse_dictionary )