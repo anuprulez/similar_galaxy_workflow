10c5a259c476e044fc4a78d32815d7563bc7dc4f
anup kumar <anup.rulez@gmail.com>
2018-06-09 19:12:04 +0200

    Update workflow extraction

--- extract_workflow_connections.py
+++ extract_workflow_connections.py
@@ -1,6 +1,7 @@
 """
 Extract steps, tools and input and output types of workflows.
 """
+
 import sys
 import os
 import json
@@ -70,35 +71,33 @@ class ExtractWorkflowConnections:
             workflow_paths.extend( flow_paths )
         print( "Workflows processed" )
         print( "All paths: %d" % len( workflow_paths ) )
+        path_counter = 0
         for path in workflow_paths:
             path_names = ",".join( path )
             if path_names not in workflow_paths_freq:
                   workflow_paths_freq[ path_names ] = 0
             workflow_paths_freq[ path_names ] += 1
+            path_counter += 1
+            print( "Path counted: %d" % path_counter )
         with open( WORKFLOW_PATHS_FREQ , "w" ) as workflow_paths_freq_file:
             workflow_paths_freq_file.write( json.dumps( workflow_paths_freq ) )
         # collect duplicate paths
-        
         for path in workflow_paths:
             workflow_paths_dup += ",".join( path ) + "\n"
         with open( WORKFLOW_PATHS_FILE_DUP, "w" ) as workflows_file:
             workflows_file.write( workflow_paths_dup )
         # collect unique paths
         print( "Removing duplicate paths..." )
-        
-        for path in workflow_paths:
-            if path not in unique_paths:
-                unique_paths.append( path )
+        unique_paths = list( set( workflow_paths_dup.split( "\n" ) ) )
         print( "Unique paths: %d" % len( unique_paths ) )
-        print( "Finding compatible next tools..." )
+        '''print( "Finding compatible next tools..." )
         next_tools = self.set_compatible_next_tools( unique_paths )
         with open( COMPATIBLE_NEXT_TOOLS , "w" ) as compatible_tools_file:
-            compatible_tools_file.write( json.dumps( next_tools ) )
+            compatible_tools_file.write( json.dumps( next_tools ) )'''
         print( "Writing workflows to a text file..." )
         random.shuffle( unique_paths )
-        
         for path in unique_paths:
-            workflow_paths_unique += ",".join( path ) + "\n"
+            workflow_paths_unique += path + "\n"
         with open( WORKFLOW_PATHS_FILE, "w" ) as workflows_file:
             workflows_file.write( workflow_paths_unique )
             
@@ -109,8 +108,9 @@ class ExtractWorkflowConnections:
         """
         next_tools = dict()
         for path in workflow_paths:
-            for window in range( 0, len( path ) - 1 ):
-                current_next_tools = path[ window: window + 2 ]
+            path_split = path.split( "," )
+            for window in range( 0, len( path_split ) - 1 ):
+                current_next_tools = path_split[ window: window + 2 ]
                 current_tool = current_next_tools[ 0 ]
                 next_tool = current_next_tools[ 1 ]
                 if current_tool in next_tools:
