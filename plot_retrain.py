7753c5a8b15a594b848ccf9705b84af2a9698ae5
Anup Kumar <anup.rulez@gmail.com>
2018-12-12 13:43:01 +0100

    Add plots

--- plot_retrain.py
+++ plot_retrain.py
@@ -9,9 +9,10 @@ import numpy as np
 
 train_loss_file = "data/generated_files/mean_test_loss.txt"
 retrain_loss_file = "data/generated_files/retrain_mean_test_loss.txt"
-
+retrain_loss_no_fine_tuning = "data/generated_files/retrain_no_finetuning_mean_test_loss.txt"
 
 fontsz = 28
+circle_size = 10
 
 def read_file(file_name):
     with open(file_name) as fl:
@@ -20,8 +21,12 @@ def read_file(file_name):
 
 train_loss = read_file(train_loss_file)
 train_loss = train_loss[:len(train_loss) - 1]
+
 retrain_loss = read_file(retrain_loss_file)
 retrain_loss = retrain_loss[:len(retrain_loss) - 1]
+
+retrain_loss_no_fine_tuning = read_file(retrain_loss_no_fine_tuning)
+retrain_loss_no_fine_tuning = retrain_loss_no_fine_tuning[:len(retrain_loss_no_fine_tuning) - 1]
 
 x_axis = list(range(1, len(train_loss) + 1))
 
@@ -30,9 +35,9 @@ trace0 = go.Scatter(
     x = x_axis,
     y = train_loss,
     mode = 'lines+markers',
-    name = 'Training loss',
+    name = 'Training (base)',
     marker = dict(
-        size = 12,
+        size = circle_size,
         color='rgb(243, 1, 1)',
         line = dict(
             width = 2,
@@ -45,9 +50,9 @@ trace1 = go.Scatter(
     x = x_axis,
     y = retrain_loss,
     mode = 'lines+markers',
-    name = 'Re-training loss',
+    name = 'Re-training (fine tuning)',
     marker = dict(
-        size = 12,
+        size = circle_size,
         color='rgba(50, 171, 96, 1.0)',
         line = dict(
             width = 2,
@@ -56,8 +61,24 @@ trace1 = go.Scatter(
     )
 )
 
+trace2 = go.Scatter(
+    x = x_axis,
+    y = retrain_loss_no_fine_tuning,
+    mode = 'lines+markers',
+    name = 'Re-training (no fine tuning)',
+    marker = dict(
+        size = circle_size,
+        color='rgb(0,191,255)',
+        line = dict(
+            width = 2,
+            color = 'rgb(0,191,255)'
+        )
+    )
+)
+
+
 layout_time_acc = dict(
-    title='Cross-entropy loss for training and re-training',
+    title='Cross-entropy loss comparison for training and re-training with fine tuning',
     font=dict(family='Times new roman', size=fontsz),
     yaxis=dict(
         showgrid=True,
@@ -88,13 +109,14 @@ layout_time_acc = dict(
     ),
 )
 
-fig_tp = go.Figure(data=[trace0, trace1], layout=layout_time_acc)
+fig_tp = go.Figure(data=[trace0, trace1, trace2], layout=layout_time_acc)
 pio.write_image(fig_tp, 'data/generated_files/loss_train_retrain.png', width=1200, height=800)
 
 #### Plot precision 
 
 train_precision_file = "data/generated_files/mean_test_absolute_precision.txt"
 retrain_precision_file = "data/generated_files/retrain_mean_test_absolute_precision.txt"
+retrain_precision_no_fine_tuning = "data/generated_files/retrain_no_finetuning_test_absolute_precision.txt"
 
 train_precision = read_file(train_precision_file)
 train_precision = train_precision[:len(train_precision) - 1]
@@ -102,14 +124,17 @@ train_precision = train_precision[:len(train_precision) - 1]
 retrain_precision = read_file(retrain_precision_file)
 retrain_precision = retrain_precision[:len(retrain_precision) - 1]
 
+retrain_precision_no_fine_tuning = read_file(retrain_precision_no_fine_tuning)
+retrain_precision_no_fine_tuning = retrain_precision_no_fine_tuning[:len(retrain_precision_no_fine_tuning) - 1]
+
 # Create traces
 trace0 = go.Scatter(
     x = x_axis,
     y = train_precision,
     mode = 'lines+markers',
-    name = 'Training precision',
+    name = 'Training (base)',
     marker = dict(
-        size = 12,
+        size = circle_size,
         color='rgb(243, 1, 1)',
         line = dict(
             width = 2,
@@ -122,9 +147,9 @@ trace1 = go.Scatter(
     x = x_axis,
     y = retrain_precision,
     mode = 'lines+markers',
-    name = 'Re-training precision',
+    name = 'Re-training (fine tuning)',
     marker = dict(
-        size = 12,
+        size = circle_size,
         color='rgba(50, 171, 96, 1.0)',
         line = dict(
             width = 2,
@@ -133,14 +158,30 @@ trace1 = go.Scatter(
     )
 )
 
+trace2 = go.Scatter(
+    x = x_axis,
+    y = retrain_precision_no_fine_tuning,
+    mode = 'lines+markers',
+    name = 'Re-training (no fine tuning)',
+    marker = dict(
+        size = circle_size,
+        color='rgb(0, 191, 255)',
+        line = dict(
+            width = 2,
+            color = 'rgb(0, 191, 255)'
+        )
+    )
+)
+
 layout_time_acc = dict(
-    title='Precision for training and re-training',
+    title='Accuracy comparison for training and re-training with fine tuning',
     font=dict(family='Times new roman', size=fontsz),
     yaxis=dict(
         showgrid=True,
         showline=True,
         showticklabels=True,
-        title='Precision',
+        range=[0.85, 1],
+        title='Accuracy',
         titlefont=dict(
             family='Times new roman',
             size=fontsz
@@ -165,6 +206,6 @@ layout_time_acc = dict(
     ),
 )
 
-fig_tp = go.Figure(data=[trace0, trace1], layout=layout_time_acc)
+fig_tp = go.Figure(data=[trace0, trace1, trace2], layout=layout_time_acc)
 pio.write_image(fig_tp, 'data/generated_files/precision_train_retrain.png', width=1200, height=800)
 
