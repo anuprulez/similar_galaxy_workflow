9df812e50393c4a0cc0ac65884631cae932e1a5c
Anup Kumar <anup.rulez@gmail.com>
2019-04-02 15:19:40 +0200

    Configure mongo path

--- optimise_hyperparameters.py
+++ optimise_hyperparameters.py
@@ -87,8 +87,8 @@ class HyperparameterOptimisation:
                 callbacks=[early_stopping]
             )
             return {'loss': model_fit.history["val_loss"][-1], 'status': STATUS_OK}
-        # trials = Trials()
-        trials = MongoTrials('mongo://localhost:1234/foo_db7/jobs')
+        mongo_path = 'mongo://' + config["host_port"] + '/' + config["db_name"] + '/jobs'
+        trials = MongoTrials(mongo_path)
         # minimize the objective function using the set of parameters above
         learned_params = fmin(create_model, params, trials=trials, algo=tpe.suggest, max_evals=int(config["max_evals"]))
         print(learned_params)
